1cec737c1ea80cee5b05aa20a2414d6cdd71532f
Flipapa <roamer2010x@gmail.com>
2021-07-25 01:05:43 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.use(function (req, res, next) {
+  let timeStart = new Date()
+  timeStart = timeStart.toLocaleString('zh-TW', { hour12: false })
+  console.log(timeStart, '|', req.method, 'from', req.originalUrl)
+  next()
+});
+
+app.get('/', (req, res) => {
+  const id = Math.floor(Math.random() * 999)
+  const text = '列出全部 Todo'
+  res.render('index', { text, id })
+})
+
+app.get('/new', (req, res) => {
+  const id = Math.floor(Math.random() * 999)
+  const text = '新增 Todo 頁面'
+  res.render('index', { text, id })
+})
+
+app.get('/:id', (req, res) => {
+  const id = Math.floor(Math.random() * 999)
+  const text = '顯示一筆 Todo'
+  res.render('index', { text, id })
+})
+
+app.post('/', (req, res) => {
+  const id = Math.floor(Math.random() * 999)
+  const text = '新增一筆  Todo'
+  res.render('index', { text, id })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
